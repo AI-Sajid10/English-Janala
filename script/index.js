e1cc67105ab6720656364fb34e4d45bd03fee4c1
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-05-07 14:24:03 +0600

    words added

--- script/index.js
+++ script/index.js
@@ -1,17 +1,71 @@
 const loadLesson = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
-    .then((res)=> res.json())
-    .then((data)=> {
-        displayLesson(data.data);
+        .then((res) => res.json())
+        .then((data) => {
+            displayLesson(data.data);
+        })
+}
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            displayloadLevelWord(data.data);
+        })
+}
+displayloadLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+    <div
+        class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla"
+      >
+        <img class="mx-auto" src="/assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+    
+    `;
+        /*manageSpinner(false);*/
+        return;
+    }
+    words.forEach(word => {
+        const wordDiv = document.createElement("div");
+        wordDiv.innerHTML = `<div
+        class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
+      >
+        <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"
+            }</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"
+            } / ${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"
+            }"</div> 
+        <div class="flex justify-between items-center">
+          <button onclick="loadWordDetail(${word.id
+            })" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button onclick="pronounceWord('${word.word
+            }')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>`;
+        wordContainer.appendChild(wordDiv);
     })
 }
 
 const displayLesson = (lessons) => {
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
-    for(let lesson of lessons){
+    for (let lesson of lessons) {
         const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
+        btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`;
 
         levelContainer.appendChild(btnDiv);
     }
